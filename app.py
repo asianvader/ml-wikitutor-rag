b93d6177b275aed59dfd3734790733ea5ae9b97b
Phoebe Voong-Fadel <phoebe.voong@gmail.com>
2026-02-23 19:50:00 +0000

    Initial project setup with uv, LangChain, Zvec, Streamlit

---
+++ app.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import streamlit as st
+
+st.title("ML WikiTutor")
+st.write("Hello, world 👋")
+
+st.write("If you can see this, Streamlit is working correctly.")
